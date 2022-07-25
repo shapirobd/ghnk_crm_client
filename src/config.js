714df14ebb9f88a3c98a788ebbbbb54f5c9237fd
Brian Shapiro <briandavidshapiro@gmail.com>
2022-07-24 21:10:54 -0400

    fixing config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || "http://localhost:5000";
-// export const API_URL = process.env.API_URL || "https://ghnk-crm-server.herokuapp.com";
+// export const API_URL = process.env.API_URL || "http://localhost:5000";
+export const API_URL = process.env.API_URL || "https://ghnk-crm-server.herokuapp.com";
