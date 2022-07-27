bfc97f2d012c02dac466d15d23ad897c89d65a2d
Brian Shapiro <briandavidshapiro@gmail.com>
2022-07-26 23:43:20 -0400

    more styles

--- src/components/BasicTableStyles.js
+++ src/components/BasicTableStyles.js
@@ -2,7 +2,12 @@ import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles(() => ({
 	url: {
+    transitionDuration: "0.2s",
 		textDecoration: "none",
-    color: "#00F"
+    color: "#00F",
+    "&:hover": {
+      transitionDuration: "0.2s",
+      color: "#0B5"
+    }
 	},
 }));
