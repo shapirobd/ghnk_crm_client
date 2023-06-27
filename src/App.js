49021150da7fac467f8c01e7f8bbd0341aa375f1
Brian Shapiro <briandavidshapiro@gmail.com>
2023-06-27 13:12:07 -0400

    dark mode toggle

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import RegisterPage from "./components/RegisterPage";
 import React, { useState } from "react";
 import LoginRouter from './LoginRouter'
 import { ToastContainer, toast } from "react-toastify";
+import DarkModeToggle from "react-dark-mode-toggle";
 
 function App() {
 	const user = useSelector((state) => state.user);
 	const [pageName, setPageName] = useState("Home");
+	const [isDarkMode, setIsDarkMode] = useState(() => false);
 	console.log("IN APP")
 
 	return (
 		<div className="App">
 			<header
 				className={
-					(pageName === "Home" || pageName === "Shows") && user ? "App-header-align-top" : "App-header"
+					(pageName === "Home" || pageName === "Shows") && user
+						? "App-header-align-top"
+						: "App-header"
 				}
 				style={{ height: window.innerHeight }}
 			>
@@ -29,6 +33,11 @@ function App() {
 				) : (
 					<LoginRouter />
 				)}
+				<DarkModeToggle
+					onChange={setIsDarkMode}
+					checked={isDarkMode}
+					size={80}
+				/>
 			</header>
 			<ToastContainer />
 		</div>
